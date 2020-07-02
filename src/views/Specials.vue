f2368225d42ee3b0593f124ab67237bbb2c53104
michael beloff <michael.a.beloff@gmail.com>
2020-07-02 16:07:08 +1000

    add temp specials

--- src/views/Specials.vue
+++ src/views/Specials.vue
@@ -1,9 +1,67 @@
 <template>
-  <div class="container py-5">
-    <h1 class="big-title">Specials</h1>
-    <p class="sub-title">
-      <span></span>
-    </p>
+  <div class="container-fluid">
+    <div class="container py-5">
+      <h1 class="big-title">Specials</h1>
+      <p class="sub-title">
+        <span>Grab a Bargain</span>
+      </p>
+    </div>
+    <div class="container-fluid bg-light py-3">
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-12 col-md-3 bg-white py-3 my-3 mx-2 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <p class="h4 underline">
+                Discount Truck Hire
+              </p>
+              <p class="lead mt-4">
+                All storage hires over 1 month get $50/day off moving truck hire
+                to help shift your belongings.
+              </p>
+            </div>
+
+            <div class="text-right">
+              <open-modal>enquire</open-modal>
+            </div>
+          </div>
+
+          <div
+            class="col-12 col-md-3 bg-white py-3 my-3 mx-2 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <p class="h4 underline">
+                2 Months FREE
+              </p>
+              <p class="lead mt-4">
+                Pay $0 for the first 2 months when you hire for 6 months or
+                longer.
+              </p>
+            </div>
+            <div class="text-right">
+              <open-modal>enquire</open-modal>
+            </div>
+          </div>
+
+          <div
+            class="col-12 col-md-3 bg-white py-3 my-3 mx-2 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <p class="h4 underline">
+                50% Off Mobile Storage Unit
+              </p>
+              <p class="lead mt-4">
+                All hires under 6 Months get 50% off
+              </p>
+            </div>
+            <div class="text-right">
+              <open-modal>enquire</open-modal>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +69,8 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped lang="scss">
+a.gradient-slide {
+  color: inherit;
+}
+</style>
